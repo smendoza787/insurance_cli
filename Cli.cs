9b5eaa4a143b81b37c1c842f1bb671c02dc406fa
Sergio Mendoza <smendoza787@gmail.com>
2018-01-02 15:27:21 -0800

    add vertafore ascii

--- Cli.cs
+++ Cli.cs
@@ -4,6 +4,30 @@ public class Cli
 {
   public static string name;
 
+  public static void printLogo()
+  {                                                                                                  
+    Console.WriteLine();
+    Console.WriteLine();
+    Console.WriteLine("`:+/.                    `:++-          `:/+:`");
+    Console.WriteLine("/oooo+-`                 -oooo/`     `-/ooooo:");
+    Console.WriteLine("/oo//oo+:`               -ooooo+. `-/+oo/-/oo:");
+    Console.WriteLine("/oo: ./+o+:.             -oo+:/--/+oo+-`  /oo:");
+    Console.WriteLine("/oo:   `:+oo/.           -o+-.:+oo+:`     /oo:");
+    Console.WriteLine("/oo:     `-+oo+-`        `-:+oo+:-:/`     /oo:");
+    Console.WriteLine("/oo:        ./oo+:`    `:+oo+:.  :oo+.    /oo:");
+    Console.WriteLine("/oo:          ./+o+-`-/oo+/--     -+o+-   /oo:");
+    Console.WriteLine("/oo:            `--/oo+/-./++      .+oo:  /oo:");
+    Console.WriteLine("/oo:          `-/+oo+--:--oo+       `/oo/`/oo:");
+    Console.WriteLine("/oo:        ./+oo+:` ./ooooo+         :oo++oo:");
+    Console.WriteLine("/oo:     .:+oo+:.      ./+oo:          -+oooo:");
+    Console.WriteLine("/oo:  .:+oo+:.           `.`            .+ooo-");
+    Console.WriteLine("/oo/-+oo+/.                              `.-.");
+    Console.WriteLine("/ooooo/-`");
+    Console.WriteLine("./+/-`");
+    Console.WriteLine();
+    Console.WriteLine();
+  }
+
   public static void getNameAndWelcome()
   {
     Console.WriteLine("Welcome to Vertafore Insurance!");
@@ -17,11 +41,12 @@ public class Cli
 
     Customer primary_customer = new Customer(name);
 
-    // Console.WriteLine("Awesome, nice to meet you {0}", primary_customer.name);
+    Console.WriteLine("Awesome, nice to meet you {0}!", primary_customer.name);
   }
 
   public static void Main()
   {
+    printLogo();
     getNameAndWelcome();
   }
 }
