38e3bdde7264bc20daab1406727416582fe4ca59
Sergio Mendoza <smendoza787@gmail.com>
2018-01-03 02:44:24 -0800

    add constructor method to policy class

--- Policy.cs
+++ Policy.cs
@@ -2,9 +2,16 @@ using System;
 
 public class Policy
 {
+  private static int total = 0;
   private int Id;
   public int CustomerId;
   public string Type;
   public DateTime StartDate;
   public DateTime EndDate;
+
+  public Policy()
+  {
+    Id = ++total;
+    CustomerId = 1;
+  }
 }
