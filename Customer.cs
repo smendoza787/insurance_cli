a5c080a3a5a84a7c1c38ec79155dcc8a8ae9d249
Sergio Mendoza <smendoza787@gmail.com>
2018-01-02 23:21:23 -0800

    added properties to customer class

--- Customer.cs
+++ Customer.cs
@@ -2,13 +2,13 @@ using System;
 
 public class Customer
 {
-  public static int numCustomers = 0;
-  public string name;
-  public int id;
-  public Customer(string n) {
-    name = n;
-    id = ++numCustomers;
+  private static int numCustomers = 0;
+  public string First_Name;
+  public string Last_Name;
+  private int id;
 
-    Console.WriteLine("YOUR NAME IS {0} AND YOUR ID IS {1}", name, id);
+  public Customer(string n) {
+    First_Name = n;
+    id = ++numCustomers;
   }
 }
